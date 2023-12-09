67ac64f10ddb7e0ebb009100365c2184e3fb4944
Taras ihnatii <emporio862GMAIL.COM>
2023-12-09 22:38:44 +0200

    clean init.lua

--- init.lua
+++ init.lua
@@ -3,13 +3,3 @@ require("tis.lazy")
 require("Comment").setup()
 require("lualine").setup()
 
----
---vim.cmd([[
---set tabstop=4
---set softtabstop=4
---set shiftwidth=4
---set textwidth=79
---set expandtab
---set autoindent
---]])
----
