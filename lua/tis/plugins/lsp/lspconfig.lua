d3f291903f89e5e67b324e20ef297d823af1c373
Taras ihnatii <emporio862GMAIL.COM>
2023-12-11 23:27:13 +0200

    added mason, rust-tools, treesitter

--- lua/tis/plugins/lsp/lspconfig.lua
+++ lua/tis/plugins/lsp/lspconfig.lua
@@ -1,2 +1,16 @@
 return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+        -- LSP completion source:
+        "hrsh7th/cmp-nvim-lsp",
+        -- Useful completion sources:
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/vim-vsnip",
+    },
 }
---
+++ lua/tis/plugins/nvim-treesitter.lua
@@ -0,0 +1,64 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "windwp/nvim-ts-autotag",
+        },
+        config = function()
+            -- import nvim-treesitter plugin
+            local treesitter = require("nvim-treesitter.configs")
+
+            -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting=false,
+            },
+            foldmethod = "expr",
+            foldexpr = "nvim_treesitter#foldexpr()",
+            -- enable indentation
+            indent = { enable = true },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = {
+                enable = true,
+            },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "rust",
+                "toml",
+            },
+            rainbow = {
+                enable = true,
+                extended_mode = true,
+                max_file_lines = nil,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+
+      -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+      -- require('ts_context_commentstring').setup {}
+    end,
+  },
+}
